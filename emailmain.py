8a49129e87d097ff7fe8fbf475342ad0d9e3c617
Sameem <nsam603@gmail.com>
2025-02-07 13:24:38 +0530

    this is the setup

--- emailmain.py
+++ emailmain.py
@@ -1,1 +1,19 @@
-#file
+email = input('enter your email')
+k=0
+if len(email) >= 6:
+  if email[0].isalpha():
+     if ('@' in email) and (email.count('@')==1):
+       if (email[-4] =='.') ^ (email[-3]=='.'): #xzor operator ff=f,tf=t
+        for i in email:
+          if i==i.isspace():
+              k=1
+        if k==1:
+            print('wrong email space')   
+       else:
+         print('wrong .pos')     
+     else:
+       print('wrong @') 
+  else:
+    print('not valid')
+else:
+  print('wrong email 1')
